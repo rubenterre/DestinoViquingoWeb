eadbc25a48e53e3310c328eadc0b508908123824
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2024-06-14 14:36:37 +0200

    Legal page and spanish traduction

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
+
+// Sidenav
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
-        
+
+// Modal Cookies
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const cookieModal = document.querySelector('#cookies');
+    const acceptCookiesBtn = document.getElementById('acceptCookiesBtn');
+    const discardCookiesBtn = document.getElementById('discardCookiesBtn')
+
+    // Verifica se o usuario xa aceptou as cookies
+    if (!localStorage.getItem("acceptCookiesBtn")) {
+      cookieModal.style.display = "block";
+    }else{
+      cookieModal.style.display = "none"
+    }
+    
+    discardCookiesBtn.onclick = function(){
+      cookieModal.style.display = "none";
+    }
+
+    acceptCookiesBtn.onclick = function() {
+        localStorage.setItem("acceptCookiesBtn", "true");
+        localStorage.setItem("discardCookiesBtn", "true");
+        cookieModal.style.display = "none";
+    }
+});
